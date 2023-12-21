3c1ba6ca7767864902e80b21f914e4e16dd1326d
ReinadoRojo <62150172+ReinadoRojo@users.noreply.github.com>
2023-12-21 18:32:49 +0100

    fix(sidebar): fixed hydration error on sidebar

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -4,37 +4,61 @@ import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
 import { useSidebar } from "@/store/user-sidebar";
 import { Button } from "@/components/ui/button";
 import { Hint } from "@/components/hint";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const Toggle = () => {
-  const { collapsed, onCollapse, onExpand } = useSidebar((state) => state);
+  const { collapsed, onCollapse, onExpand } = useSidebar(state => state);
 
   const label = collapsed ? "Expand" : "Collapse";
 
   return (
     <>
       {collapsed && (
-        <div className="hidden lg:flex w-full items-center justify-center pt-4 mb-4">
-          <Hint label={label} side="right" asChild delayDuration={200}>
-            <Button variant={"ghost"} className="h-auto p-2" onClick={onExpand}>
-              <ArrowRightFromLine className="h-4 w-4" />
+        <div className='hidden lg:flex w-full items-center justify-center pt-4 mb-4'>
+          <Hint
+            label={label}
+            side='right'
+            asChild
+            delayDuration={200}
+          >
+            <Button
+              variant={"ghost"}
+              className='h-auto p-2'
+              onClick={onExpand}
+            >
+              <ArrowRightFromLine className='h-4 w-4' />
             </Button>
           </Hint>
         </div>
       )}
       {!collapsed && (
-        <div className="p-3 pl-6 mb-2 flex items-center w-full">
-          <p className="font-semibold text-primary">For you</p>
-          <Hint label={label} side="right" asChild delayDuration={200}>
+        <div className='p-3 pl-6 mb-2 flex items-center w-full'>
+          <p className='font-semibold text-primary'>For you</p>
+          <Hint
+            label={label}
+            side='right'
+            asChild
+            delayDuration={200}
+          >
             <Button
-              className="h-auto p-2 ml-auto"
+              className='h-auto p-2 ml-auto'
               variant={"ghost"}
               onClick={onCollapse}
             >
-              <ArrowLeftFromLine className="h-4 w-4" />
+              <ArrowLeftFromLine className='h-4 w-4' />
             </Button>
           </Hint>
         </div>
       )}
     </>
+  );
+};
+
+export const ToggleSkeleton = () => {
+  return (
+    <div className='p-3 pl-6 mb-2 hidden lg:flex items-center justify-between w-full'>
+      <Skeleton className='h-6 w-[100px]' />
+      <Skeleton className='h-6 w-6' />
+    </div>
   );
 };
--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,13 +1,28 @@
 "use client";
+import { useIsClient } from "usehooks-ts";
+
 import { useSidebar } from "@/store/user-sidebar";
 import { cn } from "@/lib/utils";
+
+import { ToggleSkeleton } from "./toggle";
+import { RecomendedSkeleton } from "./recomended";
 
 interface WrapperProps {
   children: React.ReactNode;
 }
 
 export const Wrapper = ({ children }: WrapperProps) => {
-  const { collapsed } = useSidebar((state) => state);
+  const isClient = useIsClient();
+  const { collapsed } = useSidebar(state => state);
+
+  if (!isClient) {
+    return (
+      <aside className='fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50'>
+        <ToggleSkeleton />
+        <RecomendedSkeleton />
+      </aside>
+    );
+  }
 
   return (
     <aside
